5114f08c183af913171f23bda342bfafe5738a8a
Khang Eric Truong <kilik4u@gmail.com>
2023-08-16 21:36:33 +0000

    done

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -13,6 +13,13 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+
+        StringBuilder reverse = new StringBuilder();
+
+        for (int i = str.length() - 1; i >=0; i--) {
+            reverse.append(str.charAt(i));
+        }
+
+        return reverse.toString();
     }
 }
